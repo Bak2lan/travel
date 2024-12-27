74bd3de18c109d921e14d2d55bccdfb1e611d490
Munar <muniaralbekkyzy@gmail.com>
2024-12-28 00:42:47 +0600

    add enum Role

--- src/main/java/travel/travel/model/entity/User.java
+++ src/main/java/travel/travel/model/entity/User.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
+import travel.travel.model.enums.Role;
 
 @Entity
 @Getter
@@ -23,14 +24,18 @@ public class User {
      String password;
      String phoneNumber;
 
+    @Enumerated(EnumType.STRING)
+    Role role;
+
      @OneToOne(fetch = FetchType.LAZY)
      Travel travel;
 
-    public User(String name, String email, String password, String phoneNumber, Travel travel) {
+    public User(String name, String email, String password, String phoneNumber, Role role, Travel travel) {
         this.name = name;
         this.email = email;
         this.password = password;
         this.phoneNumber = phoneNumber;
+        this.role = role;
         this.travel = travel;
     }
 }
