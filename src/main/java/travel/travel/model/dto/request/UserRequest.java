fd66a9ae7e55b6c39345eeb687408dab49949945
Your Name <youremail@domain.com>
2025-01-03 21:41:49 +0600

    valid

--- src/main/java/travel/travel/model/dto/request/UserRequest.java
+++ src/main/java/travel/travel/model/dto/request/UserRequest.java
@@ -5,11 +5,11 @@ import travel.travel.validation.password.PasswordValidation;
 import travel.travel.validation.phoneNumber.PhoneNumberValidation;
 public record UserRequest(
         String name,
-        @EmailValidation(message = "Некорректный формат email")
+        @EmailValidation
         String email,
-        @PasswordValidation(message = "Пароль должен содержать не менее 8 символов, включая цифры и специальные символы")
+        @PasswordValidation
         String password,
-        @PhoneNumberValidation(message =  " Некорректный формат номера телефона")
+        @PhoneNumberValidation
         String phoneNumber,
         Role role
 ) {
