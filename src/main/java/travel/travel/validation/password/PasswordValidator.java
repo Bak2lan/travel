4c5de9dc76eaf6848ddf4d546851633633aae248
Ismadiiar <ismadiiarerdovlatov2108@gmail.com>
2025-01-14 21:04:19 +0600

    fixed validation and data sql

--- src/main/java/travel/travel/validation/password/PasswordValidator.java
+++ src/main/java/travel/travel/validation/password/PasswordValidator.java
@@ -5,10 +5,9 @@ import jakarta.validation.ConstraintValidatorContext;
 
 public class PasswordValidator implements ConstraintValidator<PasswordValidation,String> {
 
-    private static final String PASSWORD_PATTERN =
-            "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[@#$%^&+=!]).{8,}$";
+
     @Override
     public boolean isValid(String password, ConstraintValidatorContext constraintValidatorContext) {
-        return password !=null && password.matches(PASSWORD_PATTERN);
+        return password !=null && password.length()>=8;
     }
 }
