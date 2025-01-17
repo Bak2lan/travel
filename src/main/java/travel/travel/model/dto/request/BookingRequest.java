a07b3ec17f715d1daeb6f464a6ed2f51f912a7a1
Your Name <youremail@domain.com>
2025-01-17 16:06:45 +0600

    commit

---
+++ src/main/java/travel/travel/model/dto/request/BookingRequest.java
@@ -0,0 +1,24 @@
+package travel.travel.model.dto.request;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import travel.travel.validation.email.EmailValidation;
+import travel.travel.validation.phoneNumber.PhoneNumberValidation;
+import java.time.LocalDate;
+public record BookingRequest(
+        @NotBlank(message = "First name is required")
+        String firstName,
+        @NotBlank(message = "Last name is required")
+        String lastName,
+        @EmailValidation(message = "Invalid email format")
+        @NotBlank(message = "Email is required")
+        String email,
+        @PhoneNumberValidation(message = " format" )
+        String phoneNumber,
+        @FutureOrPresent(message = "Travel dates must be in the future or present")
+        LocalDate travelDates,
+        int numberOfPeople,
+        Long tourId,
+        String message
+
+){
+}
