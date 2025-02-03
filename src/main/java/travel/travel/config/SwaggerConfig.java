835d530c6261d0b51138e9d6118be4d676a314fc
akzholbek <akzholbek0096@gmail.com>
2025-02-03 19:30:15 +0600

    Update SwaggerConfig.java

--- src/main/java/travel/travel/config/SwaggerConfig.java
+++ src/main/java/travel/travel/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,20 +18,22 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes(API_KEY, createSecurityScheme()))
-                .info(new Info()
-                        .title("Travel API")
-                        .description("API documentation for the Travel"))
-                .security(Collections.singletonList(new SecurityRequirement().addList(API_KEY)));
+            .addServersItem(new Server().url("https://api.kyrgyz-source.com").description("Production Server"))
+            .components(new Components()
+                .addSecuritySchemes(API_KEY, createSecurityScheme()))
+            .info(new Info()
+                .title("Travel API")
+                .description("API documentation for the Travel API")
+                .version("1.0.0"))
+            .security(Collections.singletonList(new SecurityRequirement().addList(API_KEY)));
     }
 
     private SecurityScheme createSecurityScheme() {
         return new SecurityScheme()
-                .name("Authorization")
-                .description("Enter your JWT token")
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("Bearer")
-                .bearerFormat("JWT");
+            .name("Authorization")
+            .description("Enter your JWT token")
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("Bearer")
+            .bearerFormat("JWT");
     }
 }
