5e263d8d6bd01ef42768aae0d69152f57e65e1be
Nurik <nalymbaide@gmail.com>
2025-03-17 21:34:52 +0600

    added new languages

--- src/main/java/travel/travel/model/entity/TourDetails.java
+++ src/main/java/travel/travel/model/entity/TourDetails.java
@@ -23,4 +23,40 @@ public class TourDetails {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "tour_id")
     private Tour tour;
+
+    private String toursDetailNameRu;
+    private String dayRu;
+    private String aboutTourDetailsRu;
+    @ElementCollection(fetch = FetchType.LAZY)
+    private List<String>imageTourDetailsRu;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tour_idRu")
+    private Tour tourRu;
+
+    private String toursDetailNameDe;
+    private String dayDe;
+    private String aboutTourDetailsDe;
+    @ElementCollection(fetch = FetchType.LAZY)
+    private List<String>imageTourDetailsDe;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tour_idDe")
+    private Tour tourDe;
+
+    private String toursDetailNameFr;
+    private String dayFr;
+    private String aboutTourDetailsFr;
+    @ElementCollection(fetch = FetchType.LAZY)
+    private List<String>imageTourDetailsFr;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tour_idFr")
+    private Tour tourFr;
+
+    private String toursDetailNameEs;
+    private String dayEs;
+    private String aboutTourDetailsEs;
+    @ElementCollection(fetch = FetchType.LAZY)
+    private List<String>imageTourDetailsEs;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tour_idEs")
+    private Tour tourEs;
 }
